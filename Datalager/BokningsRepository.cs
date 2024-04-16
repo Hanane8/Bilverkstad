57a86e1269772606b324761be5e9cf2a6b784175
Noel Stark <noel.k.stark@gmail.com>
2024-04-16 17:14:12 +0200

    uppdaterad

---
+++ Datalager/BokningsRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entitetslager.Entiteter;
+
+namespace DataLager
+{
+    public class BokningsRepository
+    {
+        private EntityFramework _dbContext;
+
+        public BokningsRepository(EntityFramework dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public List<Bokning> HämtaBokning(string personnummer)
+        {
+           return _dbContext.Bokning.Find(personnummer);
+        }
+    }
+}
---
+++ Datalager/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿namespace DataLager
+{
+    public class UnitOfWork
+    {
+        private KundRepository _kundRepository;
+        private readonly EntityFramework _dbContext;
+        private BokningsRepository _bokningsRepository;
+        public UnitOfWork(EntityFramework dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public KundRepository KundRepo
+        {
+            get
+            {
+                if (_kundRepository == null)
+                    _kundRepository = new KundRepository(_dbContext);
+                return _kundRepository;
+            }
+        }
+
+        public BokningsRepository BokningsRepo
+        {
+            get
+            {
+                if (_bokningsRepository == null)
+                    _bokningsRepository = new BokningsRepository(_dbContext);
+                return _bokningsRepository;
+            }
+        }
+
+        public void SaveChanges()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
