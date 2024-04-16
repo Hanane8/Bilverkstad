5837ae6cd2bbb27dfa363002925c50b5ba08b038
Noel Stark <noel.k.stark@gmail.com>
2024-04-16 17:14:35 +0200

    u

---
+++ Bilverkstad/PresentationLager - Vyer/Login.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace Bilverkstad.PresentationLager
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+
+        }
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState= WindowState.Minimized;
+
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void TextUser_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Btnlogin_Click(object sender, RoutedEventArgs e)
+        {                      
+            PresentationLager.Menu menuWindow = new PresentationLager.Menu();           
+            menuWindow.ShowDialog();
+
+        }
+    }
+}
