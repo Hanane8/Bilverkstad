916b34779152a577af4b6fb5f48a7ffd7e4beaf9
NoelStark <noel.k.stark@gmail.com>
2024-04-16 23:12:06 +0200

    Patched Image Pathing

--- Affärslager/obj/Debug/net8.0/Affärslager.AssemblyInfo.cs
+++ Affärslager/obj/Debug/net8.0/Affärslager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Affärslager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60f8554b9cd0fe8c50f23d2ece046e1ac55b519f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c505bb914523e511bbd54c03488885c9142ebbda")]
 [assembly: System.Reflection.AssemblyProductAttribute("Affärslager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Affärslager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
